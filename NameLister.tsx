97009189cc45aa7496b2c151945b2a83318872fb
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-02-11 11:44:49 +0300

    1 file modified

--- NameLister.tsx
+++ NameLister.tsx
@@ -5,10 +5,11 @@ type NameProps = {
 };
 
 export default function NameLister({ name }: NameProps): JSX.Element {
+  const deferredname = React.useDeferredValue(name);
   const myList = React.useMemo(() => {
     let lt = [];
     for (let i = 0; i < 20000; i++) {
-      lt.push(<div>{name}</div>);
+      lt.push(<div>{deferredname}</div>);
     }
 
     return lt;
